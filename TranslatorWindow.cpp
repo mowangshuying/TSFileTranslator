9d2419f7c2e64444cf2216854c638d7b2e6b1816
mowangshuying <1911364602@qq.com>
2026-03-11 06:55:56 +0800

    update.

--- TranslatorWindow.cpp
+++ TranslatorWindow.cpp
@@ -6,7 +6,11 @@
 #include <qscilexer.h>
 #include <qscilexerxml.h>
 #include <FluLogUtils.h>
+//#include <QDomDocument>
 //#include <QDomComment>
+#include <QXmlStreamReader>
+#include <QXmlStreamWriter>
+
 
 TranslatorWindow::TranslatorWindow(QWidget* parent /*= nullptr*/) : FluWindowKitWindow(parent)
 {
@@ -23,11 +27,13 @@ void TranslatorWindow::__initUI()
 	__initMenu();
 	
 
+	
 	/// editor;
 	m_editor = new FluScintilla(this);
+	
 	/// xml解析器;
-	auto lexer = new QsciLexerXML;
-	m_editor->setLexer(lexer);
+	//auto lexer = new QsciLexerXML;
+	//m_editor->setLexer(lexer);
 	setCentralWidget(m_editor);
 }
 
@@ -46,6 +52,12 @@ void TranslatorWindow::__initMenu()
 	m_fileMenu->addAction(m_saveAsFileAction);
 	m_fileMenu->addAction(m_exitAction);
 
+	m_operMenu = new FluMenu;
+	m_operMenu->setTitle("Operation(&O)");
+	m_translateAction = new FluAction("Translate");
+
+	m_operMenu->addAction(m_translateAction);
+
 	m_helpMenu = new FluMenu;
 	m_helpMenu->setTitle("Help(&H)");
 
@@ -53,6 +65,7 @@ void TranslatorWindow::__initMenu()
 	m_helpMenu->addAction(m_aboutAction);
 
 	m_menuBar->addAction(m_fileMenu->menuAction());
+	m_menuBar->addAction(m_operMenu->menuAction());
 	m_menuBar->addAction(m_helpMenu->menuAction());
 }
 
@@ -60,12 +73,198 @@ void TranslatorWindow::__connect()
 {
 	connect(m_openFileAction, &FluAction::triggered, this, &TranslatorWindow::__onTriggerOpenFile);
 	connect(m_aboutAction, &FluAction::triggered, this, &TranslatorWindow::__onTriggerAbout);
+	connect(m_translateAction, &FluAction::triggered, this, &TranslatorWindow::__onTriggerTranslate);
 }
 
-void TranslatorWindow::__translate()
+//void TranslatorWindow::__translate()
+//{
+//	__Xml xml = __read(m_xmlFilePath);
+//	__write("__write.xml", xml);
+//}
+
+__Xml TranslatorWindow::__read(QString filepath)
 {
-	//// 将xml转为相应结构体;
-	LOG_DEBUG << "Xml to struct.";
+	__Xml xml;
+	Context context;
+	Message message;
+	Location location;
+
+	if (filepath.isEmpty())
+	{
+		return xml;
+	}
+
+	QFile file(filepath);
+	if (!file.open(QIODevice::ReadOnly))
+	{
+		return xml;
+	}
+
+	QXmlStreamReader reader(&file);
+	while (!reader.atEnd())
+	{
+		reader.readNext();
+		if (reader.isStartElement())
+		{
+			QString tagName = reader.name().toString();
+			if (tagName == "TS")
+			{
+				QXmlStreamAttributes attr = reader.attributes();
+				if (attr.hasAttribute("version"))
+				{
+					xml.version = attr.value("version").toString();
+				}
+				continue;
+			}
+
+			if (tagName == "context")
+			{
+				//context.clear();
+				continue;
+			}
+
+			if (tagName == "name")
+			{
+				context.name = reader.readElementText();
+				continue;
+			}
+
+			if (tagName == "message")
+			{
+				//message.clear();
+				continue;
+			}
+
+			if (tagName == "location")
+			{
+				QXmlStreamAttributes attr = reader.attributes();
+
+
+				if (attr.hasAttribute("filename"))
+				{
+					//message.location.filename = attr.value("filename").toString();
+					location.filename = attr.value("filename").toString();
+				}
+
+				if (attr.hasAttribute("line"))
+				{
+					//message.location.line = attr.value("line").toString();
+					location.line = attr.value("line").toString();
+				}
+
+				message.locations.push_back(location);
+				continue;
+			}
+
+			if (tagName == "source")
+			{
+				message.source = reader.readElementText();
+				continue;
+			}
+
+			if (tagName == "translation")
+			{
+				QXmlStreamAttributes attrs = reader.attributes();
+				if (attrs.hasAttribute("type"))
+				{
+					message.translation.type = attrs.value("type").toString();
+				}
+				//message.translation.type = 
+			}
+
+			continue;
+		}
+
+		if (reader.isEndElement())
+		{
+			QString tagName = reader.name().toString();
+			if (tagName == "context")
+			{
+				xml.contexts.push_back(context);
+				context.clear();
+				continue;
+			}
+
+			if (tagName == "message")
+			{
+				context.messages.push_back(message);
+				message.clear();
+				continue;
+			}
+
+
+		}
+	}
+
+	file.close();
+	return xml;
+}
+
+void TranslatorWindow::__translate(__Xml& xml)
+{
+	for (auto context : xml.contexts)
+	{
+		for (auto message : context.messages)
+		{
+			QString s = message.source;
+			QString t;
+		}
+	}
+}
+
+void TranslatorWindow::__write(QString filepath, __Xml xml)
+{
+	QFile file(filepath);
+	if (!file.open(QIODevice::WriteOnly | QIODevice::Truncate))
+	{
+		return;
+	}
+
+	QXmlStreamWriter writer(&file);
+	writer.setAutoFormatting(true);
+	writer.writeStartDocument();
+
+	/// 写入ts;
+	writer.writeStartElement("TS");
+	writer.writeAttribute("version", xml.version);
+
+	for (auto context : xml.contexts)
+	{
+		writer.writeStartElement("context");
+		writer.writeTextElement("name", context.name);
+
+		for (auto message : context.messages)
+		{
+			writer.writeStartElement("message");
+
+			for (auto location : message.locations)
+			{
+				/// write location;
+				writer.writeStartElement("location");
+				writer.writeAttribute("filename", location.filename);
+				writer.writeAttribute("line", location.line);
+				writer.writeEndElement();
+			}
+
+			// write source;
+			writer.writeTextElement("source", message.source);
+			
+			///
+			writer.writeStartElement("translation");
+			writer.writeAttribute("type", message.translation.type);
+			//writer.writeTextElement("translation", message.translation.translation);
+			writer.writeCharacters(message.translation.translation);
+			writer.writeEndElement();
+
+			writer.writeEndElement();
+		}
+
+		writer.writeEndElement();
+	}
+
+	writer.writeEndElement();// 结束ts;
+
+	file.close();
 }
 
 void TranslatorWindow::__onTriggerOpenFile(bool b)
@@ -92,7 +291,19 @@ void TranslatorWindow::__onTriggerOpenFile(bool b)
 		return;
 	}
 
+	m_xmlFilePath = filepath;
+	
+	//QDomDocument doc;
+	//if (doc.setContent(&file))
+	//{
+	//	file.close();
+	//	return;
+	//}
+
+	
 	QString str = file.readAll();
+
+
 	file.close();
 	m_editor->setText(str);
 }
@@ -101,4 +312,10 @@ void TranslatorWindow::__onTriggerAbout(bool b)
 {
 	FluMessageBox info("About", "TS File Translator\r\nVersion: 1.0.0", this);
 	info.exec();
+}
+
+void TranslatorWindow::__onTriggerTranslate(bool b)
+{
+	__Xml xml = __read(m_xmlFilePath);
+	__write("__write.xml", xml);
 }
--- TranslatorWindow.h
+++ TranslatorWindow.h
@@ -6,35 +6,62 @@
 #include <FluMenuBar.h>
 #include <FluAction.h>
 #include <list>
+#include <QString>
 
 class Location {
 public:
-    std::string filename;
-    std::string line;
+    void clear()
+    {
+        filename.clear();
+        line.clear();
+    }
+public:
+    QString filename;
+    QString line;
 };
 
 class Translation
 {
 public:
-    std::string type;
-    std::string translation;
+    void clear()
+    {
+        type = "";
+        translation = "";
+    }
+public:
+    QString type;
+    QString translation;
 };
 
 class Message {
 public:
-    Location location;
-    std::string source;
+    void clear()
+    {
+        locations.clear();
+        source = "";
+        translation.clear();
+    }
+public:
+    std::list<Location> locations;
+    QString source;
     Translation translation;
 };
 
 class Context {
 public:
-    std::string name;
-    std::list<std::string> messages;
+    void clear()
+    {
+        name = "";
+        messages.clear();
+    }
+public:
+    QString name;
+    std::list<Message> messages;
 };
 
 class __Xml {
 public:
+    QString version;
     std::list<Context> contexts;
 };
 
@@ -50,23 +77,35 @@ public:
 
     void __connect();
 
-    void __translate();
+    //void __translate();
+
+    __Xml __read(QString filepath);
+    void __translate(__Xml& xml);
+    void __write(QString filepath, __Xml xml);
 public slots:
     void __onTriggerOpenFile(bool b = false);
     void __onTriggerAbout(bool b = false);
+    void __onTriggerTranslate(bool b = false);
 protected:
     
     ///
     FluMenu* m_fileMenu;
+    FluMenu* m_operMenu;
     FluMenu* m_helpMenu;
 
     FluAction* m_openFileAction;
     FluAction* m_saveFileAction;
     FluAction* m_saveAsFileAction;
     FluAction* m_exitAction;
+
+    FluAction* m_translateAction;
+
     FluAction* m_aboutAction;
 
 
     // editor;
 	FluScintilla* m_editor;
+
+    // 
+    QString m_xmlFilePath;
 };
