ded3ce7025ed72161c32e36497ec009585cc56c9
mowangshuying <1911364602@qq.com>
2026-03-12 05:56:55 +0800

    update.

--- TranslatorWindow.cpp
+++ TranslatorWindow.cpp
@@ -11,35 +11,52 @@
 #include <QXmlStreamReader>
 #include <QXmlStreamWriter>
 #include <TongYiOpenAi.hpp>
+//#include <QThread>
+#include <thread>
+#include <FluInfoBarMgr.h>
+#include <FluShortInfoBar.h>
+#include <QApplication>
+#include <FluDockManager.h>
+#include <FluLabel.h>
+#include <FluLineEdit.h>
+#include <FluProgressRing.h>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <FluComboBoxEx.h>
 #include <QThread>
-
+#include <FluUtils.h>
 
 
 TranslatorWindow::TranslatorWindow(QWidget* parent /*= nullptr*/) : FluWindowKitWindow(parent)
 {
-	TongYiOpenAi::__init("https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions",
-		"sk-c43463e3702d452fa7d873efb7808eae");
+	m_translateState = TranslateState::Ready;
+
+	loadConfig();
+	TongYiOpenAi::__init(m_httpurl.toStdString(), m_token.toStdString());
 
 	__initUI();
-
 	__connect();
+}
+
+void TranslatorWindow::loadConfig()
+{
+	FluConfigUtils::getUtils()->getSettings()->beginGroup("config");
+	m_httpurl = FluConfigUtils::getUtils()->getSettings()->value("httpurl", "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions").toString();
+	m_token = FluConfigUtils::getUtils()->getSettings()->value("token", "sk-******").toString();
+	m_sourceLang = FluConfigUtils::getUtils()->getSettings()->value("sourceLang", "English").toString();
+    m_targetLang = FluConfigUtils::getUtils()->getSettings()->value("targetLang", "Chinese").toString();
+	FluConfigUtils::getUtils()->getSettings()->endGroup();
 }
 
 void TranslatorWindow::__initUI()
 {
 	setWindowTitle("TS File Translator");
-	
+
 	/// menu;
 	__initMenu();
 	
-
-	
 	/// editor;
 	m_editor = new FluScintilla(this);
-	
-	/// xml解析器;
-	//auto lexer = new QsciLexerXML;
-	//m_editor->setLexer(lexer);
 	setCentralWidget(m_editor);
 }
 
@@ -49,11 +66,14 @@ void TranslatorWindow::__initMenu()
 	m_fileMenu->setTitle("File(&F)");
 
 	m_openFileAction = new FluAction("Open(&O)");
+	m_openConfigFileAction = new FluAction("Open Config File...");
+
 	m_saveFileAction = new FluAction("Save(&S)");
 	m_saveAsFileAction = new FluAction("Save As");
 	m_exitAction = new FluAction("Exit");
 	
 	m_fileMenu->addAction(m_openFileAction);
+	m_fileMenu->addAction(m_openConfigFileAction);
 	m_fileMenu->addAction(m_saveFileAction);
 	m_fileMenu->addAction(m_saveAsFileAction);
 	m_fileMenu->addAction(m_exitAction);
@@ -78,8 +98,12 @@ void TranslatorWindow::__initMenu()
 void TranslatorWindow::__connect()
 {
 	connect(m_openFileAction, &FluAction::triggered, this, &TranslatorWindow::__onTriggerOpenFile);
+    connect(m_openConfigFileAction, &FluAction::triggered, this, &TranslatorWindow::__onTriggerOpenConfigFile);
+    connect(m_saveFileAction, &FluAction::triggered, this, &TranslatorWindow::__onTriggerSaveFile);
 	connect(m_aboutAction, &FluAction::triggered, this, &TranslatorWindow::__onTriggerAbout);
 	connect(m_translateAction, &FluAction::triggered, this, &TranslatorWindow::__onTriggerTranslate);
+	connect(m_exitAction, &FluAction::triggered, this, &TranslatorWindow::__onTriggerExit);
+	connect(this, &TranslatorWindow::__translateValueChanged, this, &TranslatorWindow::__onTranslateValueChanged);
 }
 
 //void TranslatorWindow::__translate()
@@ -230,23 +254,51 @@ QString TranslatorWindow::__translate(QString sourceLang, QString targetLang, QS
 
 
 	auto _json = TongYiOpenAi::completion().create(json);
-	LOG_DEBUG << "_json:" << _json.dump().c_str();
+	//LOG_DEBUG << "_json:" << _json.dump().c_str();
 	//return QString(_json["choices"][0]["message"]["content"]);
-	std::string content =  _json["choices"][0]["message"]["content"];
+
+	// _json is empty
+    if (_json.empty())
+    {
+        return "";
+    }
+
+	// _json has choices
+    if (!_json.contains("choices") || !_json["choices"].is_array())
+    {
+        return "";
+    }
+
+	std::string content =  _json["choices"][0]["message"]["content"];	
 	QString dest =  QString::fromUtf8(content);
 	return dest;
 }
 
 void TranslatorWindow::__translate(__Xml& xml)
 {
+	int nTotalTranslate = 0;
+	int nTranslate = 0;
+
+	int p = 0;
+
+	for (auto& context : xml.contexts)
+	{
+		for (auto& message : context.messages)
+		{
+			nTotalTranslate += 1;
+		}
+	}
+
 	for (auto& context : xml.contexts)
 	{
 		for (auto& message : context.messages)
 		{
 			QString s = message.source;
-			QString t = __translate("English", "Japanese", s);
-			QThread::msleep(5000);
+			QString t = __translate(m_sourceLang, m_targetLang, s);
+			QThread::msleep(1500);
 			message.translation.translation = t;
+			nTranslate += 1;
+			emit __translateValueChanged(nTranslate, nTotalTranslate);
 		}
 	}
 }
@@ -290,7 +342,7 @@ void TranslatorWindow::__write(QString filepath, __Xml xml)
 			
 			///
 			writer.writeStartElement("translation");
-			writer.writeAttribute("type", message.translation.type);
+			//writer.writeAttribute("type", message.translation.type);
 			//writer.writeTextElement("translation", message.translation.translation);
 			writer.writeCharacters(message.translation.translation);
 			writer.writeEndElement();
@@ -304,6 +356,22 @@ void TranslatorWindow::__write(QString filepath, __Xml xml)
 	writer.writeEndElement();// 结束ts;
 
 	file.close();
+}
+
+void TranslatorWindow::closeEvent(QCloseEvent* event)
+{
+	FluMessageBox messageBox(tr("Close TSFileTranslator?"), tr("choose \"Ok\" to close. choose \"Cancel\" do nothing."), this);
+	int nExec = messageBox.exec();
+	if (nExec == QDialog::Rejected)
+	{
+		event->ignore();
+		return;
+	}
+	else if (nExec == QDialog::Accepted)
+	{
+		// event->accept();
+		QApplication::quit();
+	}
 }
 
 void TranslatorWindow::__onTriggerOpenFile(bool b)
@@ -347,6 +415,39 @@ void TranslatorWindow::__onTriggerOpenFile(bool b)
 	m_editor->setText(str);
 }
 
+void TranslatorWindow::__onTriggerOpenConfigFile(bool b)
+{
+	// open config file;
+	QString filepath = "../config/config.ini";
+	m_curFilePath = filepath;
+	QFile file(filepath);
+    if (!file.open(QIODevice::ReadOnly))
+    {
+        return;
+    }
+    QString str = file.readAll();
+	m_editor->setText(str);
+    file.close();
+}
+
+void TranslatorWindow::__onTriggerSaveFile(bool b)
+{
+	// save file;
+    QFile file(m_curFilePath);
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Truncate))
+    {
+        return;
+    }
+    file.write(m_editor->text().toUtf8());
+    file.close();
+
+	if (m_curFilePath == "../config/config.ini")
+	{
+		loadConfig();
+		TongYiOpenAi::__init(m_httpurl.toStdString(), m_token.toStdString());
+	}
+}
+
 void TranslatorWindow::__onTriggerAbout(bool b)
 {
 	FluMessageBox info("About", "TS File Translator\r\nVersion: 1.0.0", this);
@@ -355,7 +456,51 @@ void TranslatorWindow::__onTriggerAbout(bool b)
 
 void TranslatorWindow::__onTriggerTranslate(bool b)
 {
-	__Xml xml = __read(m_xmlFilePath);
-	__translate(xml);
-	__write("__write.xml", xml);
+	//FluThread thread;
+	//thread.start(nullptr, [=](&thread) {
+	//	}, nullptr);
+	if (m_sourceLang == "" || m_targetLang == "")
+	{
+        FluInfoBarMgr::showInfoBar(window(), FluShortInfoBarType::Warn, "Please set source language and target language.");
+		return;
+	}
+
+	if (m_translateState == TranslateState::Working)
+	{
+		FluInfoBarMgr::showInfoBar(window(), FluShortInfoBarType::Warn, "Wait for task end.");
+		return;
+	}
+
+	std::thread __thread([=]() {
+		emit __translateStart();
+		m_translateState = TranslateState::Working;
+
+		__Xml xml = __read(m_xmlFilePath);
+		__translate(xml);
+		__write("__write.xml", xml);
+		
+
+		m_translateState = TranslateState::Ready;
+		emit __translateEnd();
+
+		});
+
+	__thread.detach();
+
+	FluMessageBox messageBox(tr("Translate Progress."), tr(""), this);
+	connect(this, &TranslatorWindow::__translateProgress, &messageBox, &FluMessageBox::setInfo);
+	messageBox.exec();
+
+}
+
+void TranslatorWindow::__onTriggerExit(bool b)
+{
+	close();
+}
+
+void TranslatorWindow::__onTranslateValueChanged(int c, int t)
+{
+	//LOG_DEBUG << c << "/" << t;
+	QString p = QString::asprintf("Translated (%d) of total (%d).", c, t);
+	emit __translateProgress(p);
 }
--- TranslatorWindow.h
+++ TranslatorWindow.h
@@ -7,6 +7,8 @@
 #include <FluAction.h>
 #include <list>
 #include <QString>
+//#include <FluThread.h>
+//#include <FluDockManager.h>
 //#include <TongYiOpenAi.hpp>
 
 class Location {
@@ -66,12 +68,18 @@ public:
     std::list<Context> contexts;
 };
 
+enum class TranslateState {
+    Ready,
+    Working,
+};
 
 class TranslatorWindow : public FluWindowKitWindow
 {
     Q_OBJECT
 public:
     TranslatorWindow(QWidget *parent = nullptr);
+
+    void loadConfig();
 
     void __initUI();
 	void __initMenu();
@@ -85,18 +93,32 @@ public:
     QString __translate(QString sourceLang, QString targetLang, QString source);
     void __translate(__Xml& xml);
     void __write(QString filepath, __Xml xml);
+
+    void closeEvent(QCloseEvent* event);
+signals:
+    void __translateStart();
+    void __translateEnd();
+    void __translateValueChanged(int c, int t);
+    void __translateProgress(QString p);
 public slots:
     void __onTriggerOpenFile(bool b = false);
+    void __onTriggerOpenConfigFile(bool b = false);
+    void __onTriggerSaveFile(bool b = false);
     void __onTriggerAbout(bool b = false);
     void __onTriggerTranslate(bool b = false);
+
+    void __onTriggerExit(bool b = false);
+
+    void __onTranslateValueChanged(int c, int t);
 protected:
     
-    ///
+
     FluMenu* m_fileMenu;
     FluMenu* m_operMenu;
     FluMenu* m_helpMenu;
 
     FluAction* m_openFileAction;
+    FluAction* m_openConfigFileAction;
     FluAction* m_saveFileAction;
     FluAction* m_saveAsFileAction;
     FluAction* m_exitAction;
@@ -111,4 +133,14 @@ protected:
 
     // 
     QString m_xmlFilePath;
+
+    TranslateState m_translateState;
+    QString m_curFilePath;
+
+
+    //// settings;
+    QString m_httpurl;
+    QString m_token;
+    QString m_sourceLang;
+    QString m_targetLang;
 };
